e7fbaebfd95d265cab57d5eafe237b3c67a08784
Cristian Sanchez <cdsanchezm@unbosque.edu.co>
2021-04-29 20:16:54 -0500

    dir

--- src/main/java/com/example/taller_4/login.java
+++ src/main/java/com/example/taller_4/login.java
@@ -1,2 +1,32 @@
-package com.example.taller_4;public class login {
-}
+package com.example.taller_4;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//public class login {
+  //@WebServlet(name = "login",value = "/login")
+  //public class Login extends HttpServlet {
+
+      //public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+          //response.setContentType("text/html");
+
+          //String name = request.getParameter("userName");
+
+          //PrintWriter out = response.getWriter();
+          //if(!name.isEmpty())
+            //  out.println("eren se vuelve");
+          //else
+        //      out.println("paloma");
+
+      //    out.close();
+
+    //  }
+
+  //}
+//}
