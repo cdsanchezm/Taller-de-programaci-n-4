39f4c9ca39a33d9ce717fb6aba1d267df99aef4d
AndresGB <agalvisb@unbosque.edu.co>
2021-04-30 19:01:05 -0500

    Hello world

--- src/main/java/com/example/taller_4/login.java
+++ src/main/java/com/example/taller_4/login.java
@@ -1,32 +1,30 @@
 package com.example.taller_4;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//public class login {
-  //@WebServlet(name = "login",value = "/login")
-  //public class Login extends HttpServlet {
+@WebServlet(name = "login", value = "/login")
+public class login extends HttpServlet {
 
-      //public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-          //response.setContentType("text/html");
+        response.setContentType("text/html");
 
-          //String name = request.getParameter("userName");
+        String name = request.getParameter("userName");
+        String password = request.getParameter("userPassword");
 
-          //PrintWriter out = response.getWriter();
-          //if(!name.isEmpty())
-            //  out.println("eren se vuelve");
-          //else
-        //      out.println("paloma");
+        PrintWriter out = response.getWriter();
+        if(name.equals("unbosque") && password.equals("123456"))
+            out.println("Login Success...!");
+        else
+            out.println("Login Failed...!");
 
-      //    out.close();
+        out.close();
 
-    //  }
+    }
 
-  //}
-//}
+}
