04e8dc6140778c6bc4a3fc0cc18a553a55d12962
Cristian Sanchez <cdsanchezm@unbosque.edu.co>
2021-05-01 12:48:12 -0500

    servelt

--- src/main/java/com/example/taller_4/login.java
+++ src/main/java/com/example/taller_4/login.java
@@ -1,29 +1,27 @@
 package com.example.taller_4;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "login", value = "/login")
+@WebServlet(name = "login", value = "/Login")
 public class login extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         response.setContentType("text/html");
 
-        String name = request.getParameter("userName");
-        String password = request.getParameter("userPassword");
+        String userName = request.getParameter("userName");
 
         PrintWriter out = response.getWriter();
-        if(name.equals("unbosque") && password.equals("123456"))
-            out.println("Login Success...!");
-        else
-            out.println("Login Failed...!");
+        out.println(userName);
+        out.println("lo logre");
+        Cookie name = new Cookie("userName", userName);
 
-        out.close();
 
     }
 
